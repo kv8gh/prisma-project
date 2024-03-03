35aa85191e24b5335e76b5cb6792db3dda10d71b
Isha <ishushree19@gmail.com>
2024-03-03 17:19:41 +0530

    added post and get functionality

--- app/api/newpost/route.js
+++ app/api/newpost/route.js
@@ -1,7 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request){
-    const data = await request.json();
-    console.log({data});
-    return NextResponse(data);
+    const data = await request.json()
+    const {name, id} = data;
+    console.log({data})
+
+    const result = await prisma.User.create({
+        data: {
+            name: name,
+            id: id
+        }
+    })
+    return NextResponse.json({data: data})
 }
--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,8 +1,9 @@
-export default function Data({name,id}){
+export default function Post({id,title,content , authorName}){
     return(
         <div className="border border-black rounded px-4 py-2">
-            <h2>{name}</h2>
+            <h2>{title}</h2>
             <p>ID: {id}</p>
+            <h2>{authorName}</h2>
         </div>
     )
 }
