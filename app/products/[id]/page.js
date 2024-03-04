abb56d9dbc60294e07cb42fcf16c29aa1b1de0b4
Isha <ishushree19@gmail.com>
2024-03-04 19:16:07 +0530

    added product detail section

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,7 +1,29 @@
-export default function page({params}){
+import Navbar from '@/app/components/navbar';
+import ProductDetailCard from "@/app/components/ProductDetailCard";
+
+async function getProduct(id){
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+    if(!response.ok) throw new Error('Network response was not ok');
+    return response.json();
+}
+
+export default async function page({params}){
+
+    const item = await getProduct(params.id);
+    console.log(item);
     return(
         <main>
-            ID: {params.id}<br/>
+            <Navbar />
+            <ProductDetailCard 
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                rating={item.rating}
+            />
+            {/* ID: {params.id}<br/> */}
         </main>
     )
 }
